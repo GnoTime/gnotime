45467c3dbb7c9f312a8e70364584deae811a961e
Linas Vepstas <linas@linas.org>
2002-09-06 22:48:47 +0000

    hacks to try to make it compile
    git-svn-id: https://gttr.svn.sourceforge.net/svnroot/gttr/trunk/gnotime@11 1e98219e-8636-0410-870a-f42bf3953145

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
+srcdir=`dirname $0` 
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="GnoTime"
@@ -10,13 +10,12 @@ PKG_NAME="GnoTime"
   && test -f $srcdir/ChangeLog \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gnome directory"
+    echo " top-level gnotime directory"
     exit 1
 }
 
-
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common"
     exit 1
 }
 USE_GNOME2_MACROS=1 . gnome-autogen.sh
