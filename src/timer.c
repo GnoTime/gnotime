60344e34613eb8c53fc9780b3f31b5f0e4de7ae8
Linas Vepstas <linas@linas.org>
2002-11-15 19:09:40 +0000

    old missing credit
    git-svn-id: https://gttr.svn.sourceforge.net/svnroot/gttr/trunk/gnotime@53 1e98219e-8636-0410-870a-f42bf3953145

--- src/timer.c
+++ src/timer.c
@@ -1,5 +1,6 @@
 /*   GTimeTracker - a time tracker
  *   Copyright (C) 1997,98 Eckehard Berns
+ *   Copyright (C) 2001 Linas Vepstas <linas@linas.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
