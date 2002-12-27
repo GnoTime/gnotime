e6810e752783d6fb63006d47d1d52a181f05b29e
Linas Vepstas <linas@linas.org>
2002-12-27 01:16:14 +0000

    some cleanups, some redesign
    git-svn-id: https://gttr.svn.sourceforge.net/svnroot/gttr/trunk/gnotime@159 1e98219e-8636-0410-870a-f42bf3953145

--- ghtml/C/gtt.scm
+++ ghtml/C/gtt.scm
@@ -1,7 +1,20 @@
 ;
-; 
-; stuff
+; FILE:
+; gtt.scm
 ;
-(define (x-gtt-show-project-title) 
+; FUNCTION:
+; Miscellaneous definitions for generating reports
+;
+;
+
+(define (gtt-show-project-title) 
         (gtt-show (gtt-project-title (gtt-selected-project))))
 
+(define (gtt-show-project-desc) 
+        (gtt-show (gtt-project-desc (gtt-selected-project))))
+
+(define (gtt-show-project-notes) 
+        (gtt-show (gtt-project-notes (gtt-selected-project))))
+
+(define (gtt-show-basic-journal)
+        (gtt-show-journal (gtt-selected-project)))
