32b9720c372606f5462a6d0a340eec3b4b36e039
Linas Vepstas <linas@linas.org>
2003-04-25 14:55:57 +0000

    detabify
    git-svn-id: https://gttr.svn.sourceforge.net/svnroot/gttr/trunk/gnotime@416 1e98219e-8636-0410-870a-f42bf3953145

--- src/timer.c
+++ src/timer.c
@@ -1,20 +1,20 @@
-/*   Low-level timer callbacks & timeout handlers for GTimeTracker 
- *   Copyright (C) 1997,98 Eckehard Berns
- *   Copyright (C) 2001,2002, 2003 Linas Vepstas <linas@linas.org>
+/* Low-level timer callbacks & timeout handlers for GTimeTracker 
+ * Copyright (C) 1997,98 Eckehard Berns
+ * Copyright (C) 2001,2002, 2003 Linas Vepstas <linas@linas.org>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <config.h>
@@ -70,7 +70,7 @@ zero_on_rollover (time_t when)
 		ctree_refresh(global_ptw);
 		log_endofday();
 		year_last_reset = t1->tm_year;
-	    	day_last_reset = t1->tm_yday;
+		day_last_reset = t1->tm_yday;
 	}
 }
 
