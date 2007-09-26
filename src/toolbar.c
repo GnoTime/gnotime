90ef2636749ba0a554888d9b4048ade0d337d67d
Goedson Teixeira Paixao <goedson@debian.org>
2007-09-26 17:57:18 +0000

    Rebuild toolbar when the user toggles the show toolbar option.
    git-svn-id: https://gttr.svn.sourceforge.net/svnroot/gttr/trunk/gnotime@884 1e98219e-8636-0410-870a-f42bf3953145

--- src/toolbar.c
+++ src/toolbar.c
@@ -72,15 +72,14 @@ toolbar_set_states(void)
 
 	if (config_show_toolbar)
 	{
-		gtk_widget_show_all(GTK_WIDGET(mytbar->tbar));
-		// gnome_app_set_toolbar(GNOME_APP(app_window), mytbar->tbar);
+		update_toolbar_sections();
 	}
 	else
 	{
-		//  XXX this should hide the toolbar completely,
-		//  but it doesn't, sinse some bonobo fudge is needed.
-		gtk_widget_hide_all(GTK_WIDGET(mytbar->tbar));
-		// gnome_app_set_toolbar(GNOME_APP(app_window), mytbar->null_tbar);
+		/* Rebuild the toolbar so that it is really hidden. There should be
+		   a better way of doing this.
+		*/
+		update_toolbar_sections();
 		return;
 	}
 
@@ -146,126 +145,128 @@ build_toolbar(void)
 		mytbar->null_tbar = GTK_TOOLBAR(gtk_toolbar_new());
 	}
 
-	if (config_show_tb_new)
+	if (config_show_toolbar)
 	{
-		mytbar->new_w = gtk_toolbar_insert_stock (mytbar->tbar,
-			GTK_STOCK_NEW,
-			_("Create a New Project..."), NULL,
-			(GtkSignalFunc)new_project, NULL,
-			position++);
-		gtk_toolbar_append_space(mytbar->tbar);
-		mytbar->spa = position;
-		position ++;
+		if (config_show_tb_new)
+		{
+			mytbar->new_w = gtk_toolbar_insert_stock (mytbar->tbar,
+													  GTK_STOCK_NEW,
+													  _("Create a New Project..."), NULL,
+													  (GtkSignalFunc)new_project, NULL,
+													  position++);
+			gtk_toolbar_append_space(mytbar->tbar);
+			mytbar->spa = position;
+			position ++;
+		}
+		if (config_show_tb_ccp)
+		{
+			mytbar->cut = gtk_toolbar_insert_stock (mytbar->tbar,
+													GTK_STOCK_CUT,
+													_("Cut Selected Project"), NULL,
+													(GtkSignalFunc)cut_project, NULL,
+													position ++);
+			mytbar->copy = gtk_toolbar_insert_stock (mytbar->tbar,
+													 GTK_STOCK_COPY,
+													 _("Copy Selected Project"), NULL,
+													 (GtkSignalFunc)copy_project, NULL,
+													 position ++);
+			mytbar->paste = gtk_toolbar_insert_stock (mytbar->tbar,
+													  GTK_STOCK_PASTE,
+													  _("Paste Project"), NULL,
+													  (GtkSignalFunc)paste_project, NULL,
+													  position ++);
+			gtk_toolbar_append_space(mytbar->tbar);
+			mytbar->spb = position;
+			if (mytbar->spa) mytbar->spb--;
+			position ++;
+		}
+		if (config_show_tb_journal)
+		{
+			/* There is no true 'stock' item for journal, so
+			 * instead we draw our own button, and use a stock
+			 * image. */
+			mytbar->journal_button = toolbar_append_stock_button(mytbar->tbar,
+																 _("Activity Journal"),
+																 _("View and Edit Timestamp Logs"),
+																 GNOME_STOCK_BOOK_OPEN,
+																 (GtkSignalFunc) show_report, ACTIVITY_REPORT);
+			position ++;
+		}
+		if (config_show_tb_prop)
+		{
+			mytbar->prop_w = gtk_toolbar_insert_stock (mytbar->tbar,
+													   GTK_STOCK_PROPERTIES,
+													   _("Edit Project Properties..."), NULL,
+													   (GtkSignalFunc) menu_properties, NULL,
+													   position ++);
+		}
+		if (config_show_tb_timer)
+		{
+			/* There is no true 'stock' item for timer, so
+			 * instead we draw our own button, and use a
+			 * pair of stock images to toggle between.
+			 */
+			mytbar->timer_button_image = GTK_IMAGE(gtk_image_new());
+			gtk_image_set_from_stock (mytbar->timer_button_image,
+									  GNOME_STOCK_TIMER, GTK_ICON_SIZE_LARGE_TOOLBAR);
+			
+			mytbar->timer_button =
+				gtk_toolbar_append_item(mytbar->tbar,
+										_("Timer"),
+										_("Start/Stop Timer"),
+										NULL,
+										GTK_WIDGET(mytbar->timer_button_image),
+										(GtkSignalFunc) menu_toggle_timer, NULL);
+			position ++;
+		}
+		if (config_show_tb_calendar)
+		{
+			mytbar->calendar_w = toolbar_append_stock_button(mytbar->tbar,
+															 _("Calendar"),
+															 _("View Calendar"),
+															 GNOME_STOCK_TEXT_BULLETED_LIST,
+															 (GtkSignalFunc)edit_calendar, NULL);
+			position ++;
+		}
+		if (((config_show_tb_timer)    ||
+			 (config_show_tb_journal)  ||
+			 (config_show_tb_calendar) ||
+			 (config_show_tb_prop)     ) &&
+			((config_show_tb_pref) ||
+			 (config_show_tb_help) ||
+			 (config_show_tb_exit)))
+		{
+			gtk_toolbar_append_space(mytbar->tbar);
+			mytbar->spc = position;
+			if (mytbar->spa) mytbar->spc--;
+			if (mytbar->spb) mytbar->spc--;
+			position ++;
+		}
+		if (config_show_tb_pref)
+		{
+			mytbar->pref = gtk_toolbar_insert_stock (mytbar->tbar,
+													 GTK_STOCK_PREFERENCES,
+													 _("Edit Preferences..."), NULL,
+													 (GtkSignalFunc)menu_options, NULL,
+													 position ++);
+		}
+		if (config_show_tb_help)
+		{
+			mytbar->help = gtk_toolbar_insert_stock (mytbar->tbar,
+													 GTK_STOCK_HELP,
+													 _("User's Guide and Manual"), NULL,
+													 (GtkSignalFunc)gtt_help_popup, NULL,
+													 position ++);
+		}
+		if (config_show_tb_exit)
+		{
+			mytbar->exit = gtk_toolbar_insert_stock (mytbar->tbar,
+													 GTK_STOCK_QUIT,
+													 _("Quit GnoTime"), NULL,
+													 (GtkSignalFunc)app_quit, NULL,
+													 position ++);
+		}
 	}
-	if (config_show_tb_ccp)
-	{
-		mytbar->cut = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_CUT,
-				_("Cut Selected Project"), NULL,
-				(GtkSignalFunc)cut_project, NULL,
-				position ++);
-		mytbar->copy = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_COPY,
-				_("Copy Selected Project"), NULL,
-				(GtkSignalFunc)copy_project, NULL,
-				position ++);
-		mytbar->paste = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_PASTE,
-				_("Paste Project"), NULL,
-				(GtkSignalFunc)paste_project, NULL,
-				position ++);
-		gtk_toolbar_append_space(mytbar->tbar);
-		mytbar->spb = position;
-		if (mytbar->spa) mytbar->spb--;
-		position ++;
-	}
-	if (config_show_tb_journal)
-	{
-		/* There is no true 'stock' item for journal, so
-		 * instead we draw our own button, and use a stock
-		 * image. */
-		mytbar->journal_button = toolbar_append_stock_button(mytbar->tbar,
-				 _("Activity Journal"),
-				 _("View and Edit Timestamp Logs"),
-				 GNOME_STOCK_BOOK_OPEN,
-				 (GtkSignalFunc) show_report, ACTIVITY_REPORT);
-		position ++;
-	}
-	if (config_show_tb_prop)
-	{
-		mytbar->prop_w = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_PROPERTIES,
-				_("Edit Project Properties..."), NULL,
-				(GtkSignalFunc) menu_properties, NULL,
-				position ++);
-	}
-	if (config_show_tb_timer)
-	{
-		/* There is no true 'stock' item for timer, so
-		 * instead we draw our own button, and use a
-		 * pair of stock images to toggle between.
-		 */
-		mytbar->timer_button_image = GTK_IMAGE(gtk_image_new());
-		gtk_image_set_from_stock (mytbar->timer_button_image,
-				 GNOME_STOCK_TIMER, GTK_ICON_SIZE_LARGE_TOOLBAR);
-
-		mytbar->timer_button =
-		       gtk_toolbar_append_item(mytbar->tbar,
-				 _("Timer"),
-				 _("Start/Stop Timer"),
-				 NULL,
-				 GTK_WIDGET(mytbar->timer_button_image),
-				 (GtkSignalFunc) menu_toggle_timer, NULL);
-		position ++;
-	}
-	if (config_show_tb_calendar)
-	{
-		mytbar->calendar_w = toolbar_append_stock_button(mytbar->tbar,
-				_("Calendar"),
-				_("View Calendar"),
-				GNOME_STOCK_TEXT_BULLETED_LIST,
-				(GtkSignalFunc)edit_calendar, NULL);
-		position ++;
-	}
-	if (((config_show_tb_timer)    ||
-	     (config_show_tb_journal)  ||
-	     (config_show_tb_calendar) ||
-	     (config_show_tb_prop)     ) &&
-	    ((config_show_tb_pref) ||
-	     (config_show_tb_help) ||
-	     (config_show_tb_exit)))
-	{
-		gtk_toolbar_append_space(mytbar->tbar);
-		mytbar->spc = position;
-		if (mytbar->spa) mytbar->spc--;
-		if (mytbar->spb) mytbar->spc--;
-		position ++;
-	}
-	if (config_show_tb_pref)
-	{
-		mytbar->pref = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_PREFERENCES,
-				_("Edit Preferences..."), NULL,
-				(GtkSignalFunc)menu_options, NULL,
-				position ++);
-	}
-	if (config_show_tb_help)
-	{
-		mytbar->help = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_HELP,
-				_("User's Guide and Manual"), NULL,
-				(GtkSignalFunc)gtt_help_popup, NULL,
-				position ++);
-	}
-	if (config_show_tb_exit)
-	{
-		mytbar->exit = gtk_toolbar_insert_stock (mytbar->tbar,
-				GTK_STOCK_QUIT,
-				_("Quit GnoTime"), NULL,
-				 (GtkSignalFunc)app_quit, NULL,
-				position ++);
-	}
-
 	return GTK_WIDGET(mytbar->tbar);
 }
 
