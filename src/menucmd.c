2dbc05d3c9bfcb83d74b529c6fc32c81e377865f
Goedson Teixeira Paixao <goedson@debian.org>
2009-11-15 17:46:13 -0200

    Added license to about dialog
    Added GPL boilerplate to the About dialog.

--- src/menucmd.c
+++ src/menucmd.c
@@ -93,6 +93,22 @@ about_box(GtkWidget *w, gpointer data)
 
 	const gchar *comments =  _("GnoTime is a combination of stop-watch, diary, "
 							   " consultant billing system and todo-list manager.") ;
+
+	const gchar *license = "This program is free software; you can redistribute it and/or\n"
+		"modify it under the terms of the GNU General Public License\n"
+		"as published by the Free Software Foundation; either version 3\n"
+		"of the License, or (at your option) any later version.\n"
+		"\n"
+		"This program is distributed in the hope that it will be useful,\n"
+		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+		"GNU General Public License for more details.\n"
+		"\n"
+		"You should have received a copy of the GNU General Public License\n"
+		"along with this program; if not, write to the Free Software\n"
+		"Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA\n"
+		"02110-1301, USA.\n";
+
 	gtk_show_about_dialog (w,
 						   "version", VERSION,
 						   "program-name", GTT_APP_TITLE,
@@ -101,7 +117,8 @@ about_box(GtkWidget *w, gpointer data)
 						   "website", "http://gnotime.sourceforge.net/",
 						   "translator-credits", _("translator-credits"),
 						   "copyright", copyright,
-						   "comments", comments, 
+						   "comments", comments,
+						   "license", license,
 						   NULL);
 }
 
