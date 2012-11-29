bde0db23477cf64fb57d3a0af15e12c4155c63b5
Linas Vepstas <linasvepstas@gmail.com>
2012-11-29 17:22:29 -0600

    fix insane check that crashes the app!
    I don't get it, the old code core dumps with a G_CRITICAL
    error ... how could this ever have worked? Who made this change?

--- src/timer.c
+++ src/timer.c
@@ -174,7 +174,7 @@ stop_main_timer (void)
 void
 init_timer(void)
 {
-	g_return_if_fail (!timer_inited);
+	if (timer_inited) return;   // prevent multiple initialization
 	timer_inited = TRUE;
 
 	idle_dialog = idle_dialog_new();
